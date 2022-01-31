7d7d057e2d810e8a74d06566e9cc5d3730ed99d7
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-31 13:42:19 +0200

    refactor : remove unused declarations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Review from './Review';
 function App() {
   return <h2>reviews project setup</h2>;
--- src/Review.js
+++ src/Review.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
